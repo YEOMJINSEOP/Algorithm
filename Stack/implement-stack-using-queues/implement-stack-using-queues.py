77d2c49f646b417cf649998ef59b2a5acb1c606c
jinseop <yeomjinseop@gmail.com>
2022-07-12 22:31:39 +0900

    modify implement-stack.py

--- Stack/implement-stack-using-queues/implement-stack-using-queues.py
+++ Stack/implement-stack-using-queues/implement-stack-using-queues.py
@@ -2,19 +2,17 @@ class MyStack:
 
     def __init__(self):
         self.q = collections.deque()
-        
+
     def push(self, x: int) -> None:
         self.q.append(x)
-        
-        for i in range(len(self.q) - 1):
-            self.q.append(self.q.popleft())
-        
 
     def pop(self) -> int:
-        return self.q.popleft()
+        for i in range(len(self.q) - 1):
+            self.push(self.q.popleft())
+        return self.popleft()
 
     def top(self) -> int:
-        return self.q[0]
+        return self.q[-1]
 
     def empty(self) -> bool:
         return len(self.q) == 0
